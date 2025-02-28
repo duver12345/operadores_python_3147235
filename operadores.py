40838cbe76476100e6161e5a270c1d3ec75f328a
Jesus <jesus@outlook.com>
2025-02-28 09:05:33 -0500

    Mi operaciones con las misma jerarquia

--- operadores.py
+++ operadores.py
@@ -4,9 +4,11 @@ Los operadores en python siguen el siguiente orden jerarquico
 1. / , *, %,
 2. +, -
 3. =
+Si hay operaciones en un mismo nivel de jerarquia, 
+se resulven de izquiera a derecha
 """ 
 a = 3 
 b = 2 
 c = 1 
-y = a * b / 3 + c
+y = a ** 2 - a * b - 1 / 3 + c
 print(y) 
