107bd3b7b8bd22f4de643bb8bdbecfefeb6d2685
evelin <evelinjasbleidya@gmail.com>
2025-03-07 06:42:47 -0500

    python_2

--- operadores.py
+++ operadores.py
@@ -11,9 +11,26 @@ se resulven de izquiera a derecha
 NOTA 2: Si hay parentesis dentro de parentesis se 
 resuelve primero el parentesis interno"
 """ 
-a = 3 
-b = 2 
-c = 1 
+a = 2
+b = 3 
+c = 7 
 x = 5
-y = ((2*a+c)/7) * (a + (4*a)/c)
+h = 1
+y = c / (x + 2)/(c * a - c + 1 - b * 2)
 print(y) 
+'''
+OPERADORES RELACIONALES:
+Las operaciones aritméticas resultan en un valor númerico
+las operaciones relacionales resultan en un valor booleano:
+True Falso (V, F , si o no)
+Operadores Relacionales:
+>, <, >=, <=, !=, ==
+JERARQUIA DE OPERADORES 
+(Incluyendo los relacionales)
+1.     ()
+2.     **
+3.    *,/,%
+4.     +,-
+5. >, <, >=, <=, !=, ==
+6.      =
+'''
